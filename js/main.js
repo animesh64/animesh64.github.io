1b6b561bc6c1fe0b3c347fa4e7a1b850b9b25e9a
animesh64 <52219299+animesh64@users.noreply.github.com>
2020-07-11 06:05:38 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,128 @@
+// on clicking downloadcv btn
+$("#resume").click(() => {
+  const file_path = "../AnimeshChourasia_resume.pdf";
+  const a = document.createElement("A");
+  a.href = file_path;
+  a.download = file_path.substr(file_path.lastIndexOf("/") + 1);
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+});
+$("#cv").click(() => {
+  const file_path = "../AnimeshChourasia_resume.pdf";
+  const a = document.createElement("A");
+  a.href = file_path;
+  a.download = file_path.substr(file_path.lastIndexOf("/") + 1);
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+});
+
+!(function ($) {
+  "use strict";
+
+  //  typedJS
+  if ($(".typed").length) {
+    var typed_strings = $(".typed").data("typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+})(jQuery);
+
+// navbar
+const overlay = document.getElementById("overlay");
+const closeMenu = document.getElementById("close-menu");
+document.getElementById("open-menu").addEventListener("click", function () {
+  overlay.classList.add("show-menu");
+});
+document.getElementById("close-menu").addEventListener("click", function () {
+  overlay.classList.remove("show-menu");
+});
+
+// about
+
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 10) {
+    $(".section-about").addClass("in");
+    $(".overlay-wrap").fadeOut(400);
+  } else {
+    $(".section-about").removeClass("in");
+    $(".overlay-wrap").fadeIn(400);
+  }
+});
+
+$(document).ready(function () {
+  $(".feedback-slides").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplayHoverPause: true,
+    autoplay: true,
+    mouseDrag: true,
+    center: true,
+    margin: 30,
+    navText: [
+      "<i class='flaticon-arrow-pointing-to-left'></i>",
+      "<i class='flaticon-arrow-pointing-to-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1.2,
+      },
+      576: {
+        items: 1.7,
+      },
+      768: {
+        items: 1.7,
+      },
+      1200: {
+        items: 2.5,
+      },
+    },
+  });
+});
+/*-----------------------------------------------------------
+ * Template Name    : Justin - Personal Portfolio
+ * Author           : beingeorge
+ * Version          : 1.0
+ * Created          : Oct 2019
+ * File Description : Main Js file of the template
+ *------------------------------------------------------------
+ */
+
+!(function ($) {
+  "use strict";
+
+  /* ---------------------------------------------- /*
+  * Preloader
+  /* ---------------------------------------------- */
+
+  $(window).on("load", function () {
+    $(".loading-screen").addClass("loaded");
+  });
+
+  /* ---------------------------------------------- /*
+  * AnimateOnScroll - Init
+  /* ---------------------------------------------- */
+
+  var wow = new WOW({
+    boxClass: "wow", // animated element css class (default is wow)
+    animateClass: "animated", // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function (box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: true, // reset animation on end (default is true)
+  });
+  wow.init();
+})(window.jQuery);
